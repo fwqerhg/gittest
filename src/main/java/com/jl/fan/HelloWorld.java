a75f97a892a5d02b05ed49cc7ea7589f1104cd7a
fanwq <fanwq@kinglian.cn>
2022-06-09 10:40:14 +0800

    第一次提交

---
+++ src/main/java/com/jl/fan/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.jl.fan;
+
+/**
+ * @auther fan
+ * @Date 2022-06-09 09:59
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
