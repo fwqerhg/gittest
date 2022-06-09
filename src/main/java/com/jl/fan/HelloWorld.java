9a6d428ebf671f64ae273190093a07aa5de53065
fanwq <fanwq@kinglian.cn>
2022-06-09 10:42:51 +0800

    第2次提交

--- src/main/java/com/jl/fan/HelloWorld.java
+++ src/main/java/com/jl/fan/HelloWorld.java
@@ -6,6 +6,9 @@ package com.jl.fan;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
     }
 }
